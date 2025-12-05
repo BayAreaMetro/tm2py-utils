42f85a8385c4143d3c3b2d3cc68f11563e416b17
jdahren <jahrenholtz@bayareametro.gov>
2025-12-05 11:07:37 -0800

    Cleaning up enrollment counts

--- tm2py_utils/inputs/enrollment/enrollment_counts.py
+++ tm2py_utils/inputs/enrollment/enrollment_counts.py
@@ -121,6 +121,10 @@ def load_colleges():
 
     colleges = gpd.GeoDataFrame(colleges, geometry="geometry", crs=ANALYSIS_CRS)
 
+    print("Sum of collegeEnroll:", colleges["collegeEnroll"].sum())
+    print("Sum of comm_coll_enroll:", colleges["comm_coll_enroll"].sum())
+    print("Sum of otherCollegeEnroll:", colleges["otherCollegeEnroll"].sum())
+
     return colleges
 
 
@@ -140,11 +144,13 @@ def load_maz_shp(use_maz_orig=False):
 
     maz = maz[["MAZ_NODE", "TAZ_NODE", "geometry"]]
 
+    print(f"maz dtypes:\n{maz.dtypes}")
+
     return maz
 
 
 
-def spatil_join_schools_to_maz(schools, maz): # This is copied from job_counts and should be standardized to join any point features to maz
+def spatial_join_schools_to_maz(schools, maz): # This is copied from job_counts and should be standardized to join any point features to maz
     """
     Spatially joins school to MAZ polygons.  Performed in two steps:
     1. Attempts to join each school to a MAZ using the 'within' predicate (i.e., schools located inside a MAZ).
@@ -192,13 +198,42 @@ def summarize_enrollment_by_maz(schools_maz, maz):
     Groups by MAZ_NODE and sums all numeric columns.
     Returns a DataFrame with MAZ_NODE and summed enrollment columns.
     """
-    numeric_cols = schools_maz.select_dtypes(include='number').columns
-    enrollment_maz = schools_maz.groupby("MAZ_NODE")[numeric_cols].sum()
+    ENROLL_COLS = ['publicEnrollGradeKto8', 'privateEnrollGradeKto8', 'EnrollGradeKto8',
+    'publicEnrollGrade9to12', 'privateEnrollGrade9to12', 'EnrollGrade9to12', 
+         'comm_coll_enroll', 'collegeEnroll',  'otherCollegeEnroll', 'AdultSchEnrl']
 
+    # Only sum columns that are present in schools_maz and listed in ENROLL_COLS
+    present_enroll_cols = [col for col in ENROLL_COLS if col in schools_maz.columns]
+    enrollment_maz = schools_maz.groupby("MAZ_NODE", as_index=False)[present_enroll_cols].sum()
 
-    # Concatenate maz with jobs and maz w/o jobs so we have a full set of maz
+    # Concatenate maz with schools and maz w/o schools so we have a full set of maz
+    maz["MAZ_NODE"] = maz["MAZ_NODE"].astype(int)
+    # maz["TAZ_NODE"] = maz["TAZ_NODE"].astype(int)
+    schools_maz["MAZ_NODE"] = schools_maz["MAZ_NODE"].astype(int)
+    # schools_maz["TAZ_NODE"] = schools_maz["TAZ_NODE"].astype(int)
+
+    print(f"maz dtypes: \n{maz.dtypes}")
+    print(f"schools with maz dtypes: \n{schools_maz.dtypes}")
+
     maz_no_schools = maz[~maz["MAZ_NODE"].isin(schools_maz["MAZ_NODE"])].drop(columns="geometry")
-    enrollment_maz = pd.concat([enrollment_maz, maz_no_schools]).reset_index(drop=True)
+    enrollment_maz = pd.concat([enrollment_maz, maz_no_schools], ignore_index=True)
+
+    enrollment_maz["MAZ_NODE"] = enrollment_maz["MAZ_NODE"].astype(int)
+    # enrollment_maz["TAZ_NODE"] = enrollment_maz["TAZ_NODE"].astype(int)
+    print(f"enrollment maz after concat dtypes: \n{enrollment_maz.dtypes}")
+
+    # Ensure we have the same set of maz ids post process 
+    maz_id_in = set(maz["MAZ_NODE"])
+    enrollment_maz_ids = set(enrollment_maz["MAZ_NODE"])
+
+    # Ensure enrollment_maz has the same set of MAZ_NODE as maz_id_in
+    if enrollment_maz_ids == maz_id_in:
+        print("enrollment_maz has the same set of MAZ_NODE ids as maz_id_in.")
+    else:
+        print("enrollment_maz does NOT have the same set of MAZ_NODE ids as maz_id_in.")
+        print("Missing in enrollment_maz:", maz_id_in - enrollment_maz_ids)
+        print("Extra in enrollment_maz:", enrollment_maz_ids - maz_id_in)
+    
 
     return enrollment_maz
 
@@ -216,9 +251,9 @@ def get_enrollment_maz(write=False, use_maz_orig=False):
     maz = load_maz_shp(use_maz_orig=use_maz_orig)
 
     # Spatial join schools to maz
-    pubschls_maz = spatil_join_schools_to_maz(pubschls, maz=maz)
-    privschls_maz = spatil_join_schools_to_maz(prvschls, maz=maz)
-    colleges_maz = spatil_join_schools_to_maz(colleges, maz=maz)
+    pubschls_maz = spatial_join_schools_to_maz(pubschls, maz=maz)
+    privschls_maz = spatial_join_schools_to_maz(prvschls, maz=maz)
+    colleges_maz = spatial_join_schools_to_maz(colleges, maz=maz)
 
     # Collapse enrollment by maz
     pub_enroll_maz = summarize_enrollment_by_maz(pubschls_maz, maz)
@@ -227,21 +262,32 @@ def get_enrollment_maz(write=False, use_maz_orig=False):
 
     # Combine
     enroll_maz = pub_enroll_maz.merge(
-        priv_enroll_maz, on=["MAZ_NODE", "TAZ_NODE"], how="left"
+        priv_enroll_maz, on="MAZ_NODE", how="left", validate="1:1"
     ).merge(
-        college_enroll_maz, on=["MAZ_NODE", "TAZ_NODE"], how="left"
+        college_enroll_maz, on="MAZ_NODE", how="left", validate="1:1"
     ).fillna(0)
 
     # Add public + private enroll cols EnrollGradeKto8, EnrollGrade9to12
     enroll_maz["EnrollGradeKto8"] = enroll_maz["publicEnrollGradeKto8"] + enroll_maz["privateEnrollGradeKto8"]
     enroll_maz["EnrollGrade9to12"] = enroll_maz["publicEnrollGrade9to12"] + enroll_maz["privateEnrollGrade9to12"]
 
+    # Sort by maz
+    enroll_maz["MAZ_NODE"] = enroll_maz["MAZ_NODE"].sort_values()
+
+    # Make enroll_maz a GeoDataFrame by merging with maz geometry
+    # enroll_maz = enroll_maz.merge(maz[["MAZ_NODE", "geometry"]], on="MAZ_NODE", how="left")
+    # enroll_maz = gpd.GeoDataFrame(enroll_maz, geometry="geometry", crs=ANALYSIS_CRS)
+
     if write==True:
         if use_maz_orig:
+            # OUT_FILE = r"E:\Box\Modeling and Surveys\Development\Travel Model Two Conversion\Model Inputs\2023-tm22-dev-version-05\landuse\enrollment_maz_v2_2_2023.gpkg"
             OUT_FILE = r"E:\Box\Modeling and Surveys\Development\Travel Model Two Conversion\Model Inputs\2023-tm22-dev-version-05\landuse\enrollment_maz_v2_2_2023.csv"
         else:
+            # OUT_FILE = r"E:\Box\Modeling and Surveys\Development\Travel Model Two Conversion\Model Inputs\2023-tm22-dev-version-05\landuse\enrollment_maz_v2_5_2023.gpkg"
             OUT_FILE = r"E:\Box\Modeling and Surveys\Development\Travel Model Two Conversion\Model Inputs\2023-tm22-dev-version-05\landuse\enrollment_maz_v2_5_2023.csv"
-        enroll_maz.to_csv(OUT_FILE)        
+        print(f"Writing enrollment MAZ data to: {OUT_FILE}")
+        # enroll_maz.to_file(OUT_FILE)
+        enroll_maz.to_csv(OUT_FILE)
     return enroll_maz
 
 def main():
